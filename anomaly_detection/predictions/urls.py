86e16973ecb683e43a33bffa417aaf1997b65a1b
Guillermo Sanz <guillermo.sanz@ceab.csic.es>
2025-06-18 12:35:40 +0200

    Get metric data for the last 30 days, and TileJSONView endpoint

--- anomaly_detection/predictions/urls.py
+++ anomaly_detection/predictions/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from anomaly_detection.predictions import views
@@ -9,4 +10,13 @@ router.register('metrics', views.MetricViewSet, basename='metrics')
 
 app_name = 'metrics'
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('timeseries-json/', views.TileJSONView.as_view(), name='timeseries-json'),
+]
+
+# urlpatterns = [
+# ...
+#     views.CityAndStateTileView.get_url(),  # serve tiles at /city-and-states/<int:z>/<int:x>/<int:y>
+#     views.CityAndStateTileJSON.get_urls(tiles_urls=views.CityAndStateTileView.get_url()),  # serve tilejson at /city-and-states/tiles.json
+#     ...
+# ]

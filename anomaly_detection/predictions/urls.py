294b9e2e6536497eee78eb35961c12d6c1c61e7c
Guillermo Sanz <guillermo.sanz@ceab.csic.es>
2025-06-18 12:37:08 +0200

    removed comment

--- anomaly_detection/predictions/urls.py
+++ anomaly_detection/predictions/urls.py
@@ -13,10 +13,3 @@ app_name = 'metrics'
 urlpatterns = router.urls + [
     path('timeseries-json/', views.TileJSONView.as_view(), name='timeseries-json'),
 ]
-
-# urlpatterns = [
-# ...
-#     views.CityAndStateTileView.get_url(),  # serve tiles at /city-and-states/<int:z>/<int:x>/<int:y>
-#     views.CityAndStateTileJSON.get_urls(tiles_urls=views.CityAndStateTileView.get_url()),  # serve tilejson at /city-and-states/tiles.json
-#     ...
-# ]

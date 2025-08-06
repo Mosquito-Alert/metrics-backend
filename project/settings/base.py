6359f541977307ce9ac816fdf3b2c49616b29169
guillesanz21 <guillermo.sanz@ceab.csic.es>
2025-08-06 08:40:27 +0200

    Using timescaleDB instead of basic PostgreSQL.

--- project/settings/base.py
+++ project/settings/base.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'timescale.db.backends.postgis',
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
         'NAME': os.environ.get('POSTGRES_DB', 'metrics'),

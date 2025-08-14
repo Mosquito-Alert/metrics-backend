44cb9f7e7765442ce4884b1896a494aa2d2f86eb
guillesanz21 <guillermo.sanz@ceab.csic.es>
2025-08-14 15:02:58 +0200

    Added infinity pagination with -1 argument to page_size.

--- src/utils/pagination.py
+++ src/utils/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class StandardPagination(PageNumberPagination):
@@ -8,3 +9,29 @@ class StandardPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     page_query_param = 'page'
+
+    def paginate_queryset(self, queryset, request, view=None):
+        self.request = request
+        try:
+            page_size = int(request.query_params.get(self.page_size_query_param, self.page_size))
+        except ValueError:
+            page_size = self.page_size
+
+        if page_size == -1:
+            self.page = None
+            self.count = queryset.count()
+            self.requested_page_size = page_size
+            self.results = list(queryset)
+            return self.results
+
+        return super().paginate_queryset(queryset, request, view)
+
+    def get_paginated_response(self, data):
+        if getattr(self, "requested_page_size", None) == -1:
+            return Response({
+                'count': self.count,
+                'next': None,
+                'previous': None,
+                'results': data
+            })
+        return super().get_paginated_response(data)

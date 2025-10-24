314e815bd76a9d3131c937bfabd30227028ba410
guillesanz21 <guillermo.sanz@ceab.csic.es>
2025-10-24 11:44:19 +0200

    Reproject raster from 4326 to 3857.

--- src/metrics/tasks.py
+++ src/metrics/tasks.py
@@ -9,6 +9,7 @@ from celery import shared_task
 from django.conf import settings
 from h3ronpy.pandas.raster import rasterize_cells
 from rest_framework.exceptions import ValidationError
+from rasterio.warp import calculate_default_transform, reproject, Resampling
 
 from project.s3 import s3_client
 from src.metrics import models
@@ -153,28 +154,19 @@ def rasterize_cells_for_time_dimension(time_dimension_id: int):
         print("No MetricValues found for rasterization.")
         return
 
-    # Convert res 6 → res 5 to optimise rasterization
-    # df["h3_parent"] = df["h3_index"].apply(lambda x: h3.cell_to_parent(x, 5))
-    # Aggregate by parent index (mean temperature, or any aggregation)
-    # df_res5 = df.groupby("h3_parent", as_index=False)["value"].mean()
-    # df_res5.reset_index(drop=True, inplace=True)
-    # TODO: Fix this.
-    df["h3_parent"] = df["h3_index"]
-    df_res5 = df
-
     # Filter out cells crossing the antimeridian
-    df_res5["latlong"] = df_res5["h3_parent"].apply(lambda x: h3.cell_to_boundary(x))
+    df["latlong"] = df["h3_index"].apply(lambda x: h3.cell_to_boundary(x))
 
     def long_diff_exceeds_180(latlong_tuple):
         longs = [lon for _, lon in latlong_tuple]
         return not (max(longs) - min(longs)) > 180
-    filtered_df = df_res5[df_res5["latlong"].apply(long_diff_exceeds_180)].reset_index(drop=True)
+    filtered_df = df[df["latlong"].apply(long_diff_exceeds_180)].reset_index(drop=True)
 
     # Convert h3_index from hex string to integer
-    filtered_df["h3_parent"] = filtered_df["h3_parent"].astype(str).apply(lambda x: int(x, 16))
+    filtered_df["h3_index"] = filtered_df["h3_index"].astype(str).apply(lambda x: int(x, 16))
 
     # Convert to PyArrow arrays
-    h3_array = pa.array(filtered_df["h3_parent"])
+    h3_array = pa.array(filtered_df["h3_index"])
     val_array = pa.array(filtered_df["value"].round(1))
 
     # Rasterize
@@ -182,21 +174,55 @@ def rasterize_cells_for_time_dimension(time_dimension_id: int):
     array, transform = rasterize_cells(
         h3_array,
         val_array,
-        size=6000,
+        size=(6000, 6000),
         nodata_value=nodata_value
     )
 
-    # DELETE: # Convert to 8-bit
-    # valid_mask = array != nodata_value
-    # if np.any(valid_mask):
-    #     vmin, vmax = np.percentile(array[valid_mask], [1, 99])  # ignore outliers
-    #     scaled = np.clip((array - vmin) / (vmax - vmin) * 255, 0, 255)
-    #     array = scaled.astype(np.uint8)
-    # else:
-    #     array = np.full_like(array, fill_value=0, dtype=np.uint8)
-
     # Transform the val_array to float32
     array = array.astype(np.float32)
+
+    # Reproject to 3857
+    src_crs = "EPSG:4326"
+    dst_crs = "EPSG:3857"
+    height, width = array.shape
+    profile = {
+        "driver": "GTiff",
+        "dtype": array.dtype,
+        "count": 1,
+        "height": height,
+        "width": width,
+        "transform": transform,
+        "crs": src_crs,
+        "nodata": nodata_value,
+    }
+    dst_transform, dst_width, dst_height = calculate_default_transform(
+        src_crs, dst_crs, width, height, *rasterio.transform.array_bounds(height, width, transform)
+    )
+    dst_array = np.empty((dst_height, dst_width), dtype=array.dtype)
+    reproject(
+        source=array,
+        destination=dst_array,
+        src_transform=transform,
+        src_crs=src_crs,
+        dst_transform=dst_transform,
+        dst_crs=dst_crs,
+        resampling=Resampling.nearest
+    )
+    cog_profile = profile.copy()
+    cog_profile.update({
+        "driver": "COG",
+        "dtype": dst_array.dtype,
+        "height": dst_height,
+        "width": dst_width,
+        "count": 1,
+        "crs": dst_crs,
+        "transform": dst_transform,
+        "compress": "deflate",
+        "blockxsize": 256,
+        "blockysize": 256,
+        "tiled": True,
+        "nodata": nodata_value
+    })
 
     # Time format to YYYY-MM-DDTHH:MM
     time = time_dimension.time.strftime("%Y-%m-%dT%H:%M")
@@ -205,19 +231,8 @@ def rasterize_cells_for_time_dimension(time_dimension_id: int):
     temp_dir = os.environ.get('SHARED_TEMP_DIR', "/tmp")
     temp_tiff_path = f"{temp_dir}/{time_dimension.id}_raster.tiff"
 
-    with rasterio.open(
-        temp_tiff_path,
-        "w",
-        driver="COG",
-        height=array.shape[0],
-        width=array.shape[1],
-        count=1,
-        dtype=array.dtype,
-        crs="EPSG:4326",
-        transform=transform,
-        nodata=nodata_value
-    ) as dst:
-        dst.write(array, 1)
+    with rasterio.open(temp_tiff_path, "w", **cog_profile) as dst:
+        dst.write(dst_array, 1)
 
     key = f"rasters/{time}.tiff"
     s3_client.upload_file(temp_tiff_path, settings.S3_BUCKET_NAME, key)
